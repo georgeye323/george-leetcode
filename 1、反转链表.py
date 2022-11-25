df102f881ea165337e952e557a8ad68d2ff13f07
george0323 <13107001393@163.com>
2022-11-25 10:53:34 +0800

    列表反转

---
+++ 1、反转链表.py
@@ -0,0 +1,35 @@
+class ListNode:
+    def __init__(self, x) -> None:
+        self.val = x
+        self.next = None
+
+class Solution:
+    def ReverseList(self, head:ListNode):
+        # 定义一个虚拟节点
+        if not head:
+            return None
+        cur = head
+        pre = None
+        while cur:
+            # 断开头结点
+            temp = cur.next
+            # 头结点指向前一节点
+            cur.next = pre
+            # 指针后移
+            pre = cur
+            cur = temp
+        return pre
+
+    def ReverseList1(self, head:ListNode):
+        # 递归的解法
+        if not head or not head.next:
+            return head
+        # 反转下一节点
+        pump = self.ReverseList(head.next)
+        head.next.next = head
+        # 此时的head在最后面，需要指向空
+        head.next = None
+        return pump
+
+        
+            
